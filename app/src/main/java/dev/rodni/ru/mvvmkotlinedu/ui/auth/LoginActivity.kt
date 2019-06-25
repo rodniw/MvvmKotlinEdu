18381b046dc08c7ecf73eea9bd78e3c46b473088
rodion <rodion.prosvirnin@bk.ru>
2019-06-26 01:14:27 +0300

    ui work

--- app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/auth/LoginActivity.kt
+++ app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/auth/LoginActivity.kt
@@ -1,12 +1,13 @@
-package dev.rodni.ru.mvvmkotlinedu
+package dev.rodni.ru.mvvmkotlinedu.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.rodni.ru.mvvmkotlinedu.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/auth/SignupActivity.kt
@@ -0,0 +1,13 @@
+package dev.rodni.ru.mvvmkotlinedu.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.rodni.ru.mvvmkotlinedu.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
