1a932404a7cc41a76efcc443e5aad1cc473122f5
rodion <rodion.prosvirnin@bk.ru>
2019-06-26 12:39:10 +0300

    data binding done the main things

--- app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/auth/LoginActivity.kt
+++ app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/auth/LoginActivity.kt
@@ -2,14 +2,23 @@ package dev.rodni.ru.mvvmkotlinedu.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import dev.rodni.ru.mvvmkotlinedu.R
+import dev.rodni.ru.mvvmkotlinedu.databinding.ActivityLoginBinding
 import dev.rodni.ru.mvvmkotlinedu.util.toast
 
 class LoginActivity : AppCompatActivity(), AuthListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        //activitylogingbinding is some automatically generated class from xml activity_login
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
     }
 
     override fun onStarted() {
