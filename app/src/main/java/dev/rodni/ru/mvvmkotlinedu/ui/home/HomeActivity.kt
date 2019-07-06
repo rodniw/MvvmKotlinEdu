b20674e067043901c76571f63d615533996514b0
rodion <rodion.prosvirnin@bk.ru>
2019-07-06 23:56:43 +0300

    added navigation feature.(before device testing)

--- app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/home/HomeActivity.kt
+++ app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/home/HomeActivity.kt
@@ -2,12 +2,29 @@ package dev.rodni.ru.mvvmkotlinedu.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import dev.rodni.ru.mvvmkotlinedu.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            drawer_layout
+        )
+    }
+
 }
---
+++ app/src/main/java/dev/rodni/ru/mvvmkotlinedu/ui/home/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package dev.rodni.ru.mvvmkotlinedu.ui.home.profile
+
+import androidx.lifecycle.ViewModel;
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
